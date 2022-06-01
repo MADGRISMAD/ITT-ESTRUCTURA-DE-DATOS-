c3b4a861d4653094752d6d64f2532dd35c5063f9
Madeline Sabino <93164849+MADGRISMAD@users.noreply.github.com>
2022-05-31 17:22:23 -0700

    Add files via upload

---
+++ UNIDAD 1/programa4-ejemplo memoria dinamica/programa4-ejemplo memoria dinamica/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+public class programa4_ejemplo_memoria_dinamica
+{
+
+    // ------------ SUMA
+
+    class SUMA
+    {
+       
+        public void IMPSUM (int S1, int S2)
+        {
+
+            int RP = S1 + S2;
+
+            Console.Write("\nEl resultado de su suma es: " + RP);
+
+        }
+
+        ~SUMA()
+        {
+            Console.WriteLine("\n\nLa memoria de la clase SUMA fue liberada. ");
+        }
+
+    }
+
+    // ------------ RESTA
+
+    class RESTA
+    {
+       
+        public void IMPRES (int R1, int R2)
+        {
+
+            int RS = R1 - R2;
+
+            Console.Write("\nEl resultado de su resta es: " + RS);
+
+
+        }
+
+        ~RESTA()
+        {
+            Console.WriteLine("\n\nLa memoria de la clase RESTA fue liberada. ");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            char OPS;
+
+            do
+            {
+                Console.Write("Bienvenido al menu, por favor seleccione una de las siguientes opciones.. ");
+
+                Console.Write("\n\na) Hacer una suma. ");
+                Console.Write("\nb) Hacer una resta. ");
+                Console.Write("\nc) Salir Del Programa. ");
+
+                Console.Write("\n\nIngrese la opcion a ejecutar: ");
+                
+
+                OPS = Char.Parse(Console.ReadLine());
+
+                switch (OPS)
+                {
+                    case 'a':
+
+                        Console.Clear();
+
+                        Console.Write("Ingrese el primer valor: ");
+                        int v1 = int.Parse(Console.ReadLine());
+
+                        Console.Write("\nIngrese el segundo valor: ");
+                        int v2 = int.Parse(Console.ReadLine());
+
+                        SUMA S = new SUMA();
+
+                        S.IMPSUM(v1, v2);
+
+
+                        Console.Write("\n\nPresiona ENTER para volver al menu. ");
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        break;
+
+                    case 'b':
+
+                        Console.Clear();
+
+                        Console.Write("Ingrese el primer valor: ");
+                        int vr1 = int.Parse(Console.ReadLine());
+
+                        Console.Write("\nIngrese el segundo valor: ");
+                        int vr2 = int.Parse(Console.ReadLine());
+
+                        RESTA R = new RESTA();
+
+                       R.IMPRES(vr1, vr2);
+
+                  
+                        Console.Write("\n\nPresiona ENTER para volver al menu. ");
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        break;
+
+                    case 'c':
+
+                        Console.Write("\n\nGracias por usar el programa, pulse ENTER para salir. ");
+
+                        break;
+
+
+                    default:
+
+                        Console.Clear();
+                        Console.Write("\nLa opcion que eligio no es correcta, por favor intente de nuevo. ");
+                        Console.Write("\nPresiona ENTER para volver al menu. ");
+
+                        Console.ReadKey();
+                        Console.Clear();
+
+                        break;
+                }
+
+            } while (OPS != 'c');
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UNIDAD 1/programa5-ejemplo memoria local/programa5-ejemplo memoria local/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace programa5_ejemplo_memoria_local
+{
+    class TablaMultiplicar
+    {
+        public void CargarValor()
+        {
+            int valor;
+            string linea;
+            do
+            {
+                Console.Write("Ingrese un valor (-1 para finalizar): ");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+                if (valor != -1)
+                {
+                    Calcular(valor);
+                }
+            } while (valor != -1);
+        }
+
+        public void Calcular(int v)
+        {
+            for (int f = v; f <= v * 10; f = f + v)
+            {
+                Console.Write("\n " +  f + "\n ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            TablaMultiplicar tm = new TablaMultiplicar();
+            tm.CargarValor();
+        }
+    }
+}
